5576b40709075c834c9a81e43d2b75b9bab0d3d6
lcarbajo-xq <luis.carbajo.arguelles@gmail.com>
2025-07-14 22:16:44 +0200

    feat appwrite initial config

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: 'com.louie-dev.rn2025',
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: '687523f10008e7a293e3',
+  userCollectionId: '6875241100027b6115c4'
+}
